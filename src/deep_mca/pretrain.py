74cb8fad23f94b029479f365e0aa35dcebe60e9f
PranavThurgam05 <thurgam.pranav@gmail.com>
2026-02-14 17:20:02 -0800

    moved imports

--- src/deep_mca/pretrain.py
+++ src/deep_mca/pretrain.py
@@ -11,12 +11,12 @@ import random
 from pathlib import Path
 
 import torch
+import wandb
 import yaml
 from datasets import load_dataset
 from torch.utils.data import DataLoader, Dataset
 from transformers import MambaConfig, MambaForCausalLM
 
-import wandb
 from deep_mca.data import CollateLM
 from deep_mca.tokenizer import Tokenizer
 from deep_mca.utils import build_scheduler
